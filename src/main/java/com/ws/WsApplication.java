3348972c6c1adca1b13c82a3c2dda35174a71b43
xiebanglin <2419507265@qq.com>
2020-12-01 15:17:45 +0800

    提交websocket,springboot作为客户端，web前端，的一个聊天程序，参考resource -- file 中文件

---
+++ src/main/java/com/ws/WsApplication.java
@@ -0,0 +1,26 @@
+package com.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@SpringBootApplication
+@EnableWebSocket
+public class WsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsApplication.class, args);
+    }
+
+    /**
+     * 初始化Bean，它会自动注册使用了 @ServerEndpoint 注解声明的 WebSocket endpoint
+     *
+     * @return
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
